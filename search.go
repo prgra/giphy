8d57fac27ff1341a1ce4863fceb72964a138d5b5
prgr <me@prgr.org>
2020-12-22 12:13:41 +0300

    pagination search method

--- search.go
+++ search.go
@@ -22,3 +22,21 @@ func (c *Client) Search(args []string) (Search, error) {
 
 	return search, nil
 }
+
+// SearchPg with limit and pagination returns a search response from the Giphy API
+func (c *Client) SearchPg(args []string, page int, lim int) (Search, error) {
+	argsStr := strings.Join(args, " ")
+
+	path := fmt.Sprintf("/gifs/search?limit=%d&offset=%d&q=%s", lim, page*lim, argsStr)
+	req, err := c.NewRequest(path)
+	if err != nil {
+		return Search{}, err
+	}
+
+	var search Search
+	if _, err = c.Do(req, &search); err != nil {
+		return Search{}, err
+	}
+
+	return search, nil
+}
